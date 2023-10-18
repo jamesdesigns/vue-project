e3463de3c53c2033dadc0da1299da44a46d23c4b
James Hooper <jameshooperdesigns@gmail.com>
2023-10-18 00:02:45 -0600

    Add styles to button and herobanner

--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="hero">
     <div class="hero-content">
-      <h1>Get Hired</h1>
-      <p>Download E-book</p>
+      <h1 class="large-text">Get Hired</h1>
+      <button>Download E-book</button>
     </div>
     <div class="hero-image">
       <img src="../assets/logo.svg" alt="Image of E-book" width="200" />
@@ -20,5 +20,17 @@
     height: 70vh;
     width: 100%;
   }
+  .hero-content { text-align: center;}
+  button { 
+    padding: 20px 50px; 
+    border-radius: 50px;
+    background-color: #0099ff;
+    color: white;
+    font-size: 1.5rem;
+    border: none;
+    margin-bottom: 30px;
+    cursor: pointer;
+  }
+  .large-text { font-size: 4rem; font-weight: bold; }
 
 </style>
