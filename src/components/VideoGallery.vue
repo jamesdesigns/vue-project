87e99e15c6324e57039f4104fb0ddc58d0f685c1
James Hooper <jameshooperdesigns@gmail.com>
2023-11-18 13:41:18 -0700

    Add the Title below each video

--- src/components/VideoGallery.vue
+++ src/components/VideoGallery.vue
@@ -23,7 +23,7 @@ export default {
   data() {
     return {
       // Hardcoded YouTube video IDs
-      videoIds: ['evfFiXNfMGU?si=tZtgI8GtyKKbEhvn', 'evfFiXNfMGU?si=tZtgI8GtyKKbEhvn', 'evfFiXNfMGU?si=tZtgI8GtyKKbEhvn','TgqeRTwZvIo?si=GVf2f_d2rUW36jd7','TgqeRTwZvIo?si=GVf2f_d2rUW36jd7','TgqeRTwZvIo?si=GVf2f_d2rUW36jd7'],
+      videoIds: ['kbZejnPXyLM?si=Iu8hmadPtfMTynz3', 'SRec90j6lTY?si=aJVpNhfCt5E6vzrB', 'rYH7AErVd7w?si=_YAEGfSMLmDIStnc','t0aCoqXKFOU?si=j9IZqmkzSp8hPCbD','TgqeRTwZvIo?si=GVf2f_d2rUW36jd7','c9Wg6Cb_YlU?si=n8m_HCn0QXpmMIeC','an5LH3K44ks?si=aSOujKAemhAyoZ8c','BZ0QER_0ZWI?si=rs5Sw7yq9qYQz4cx','uL2ZB7XXIgg?si=UteNqhgxzbU7wksA','D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx','fYs2Mdyasuc?si=VpZs941UHD_oXuK_','MkKr2Cc0BuM?si=wpAAbxKViT2xP8ZR'],
       videos: [
         // Add video URLs or other information here
         { url: 'https://example.com/video1.mp4', title: 'User Research' },
@@ -46,7 +46,7 @@ export default {
     },
     methods: {
       generateYouTubeEmbedCode(videoId) {
-      return `<iframe width="350" height="200" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
+      return `<iframe width="350" height="200" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe><br><h3>Title</h3>`;
     },
   }
 };
@@ -58,7 +58,7 @@ export default {
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(5, 1fr);
   grid-column-gap: 0px;
-  grid-row-gap: 0px;
+  grid-row-gap: 35px;
   justify-items: center;
   position: relative;
   padding-bottom:5%; 
@@ -66,6 +66,7 @@ export default {
   overflow: hidden;
   width: 100%;
   margin-top:30px;
+  color: white;
 }
 @media only screen and (max-width: 1000px) {
     .video-gallery { 
