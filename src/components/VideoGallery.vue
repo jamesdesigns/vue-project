234b96a0af2f87f301d88c46281dc747088aa6d7
James Hooper <jameshooperdesigns@gmail.com>
2023-11-20 19:56:18 -0700

    Fix the looping of an object for the VideoComponent

--- src/components/VideoGallery.vue
+++ src/components/VideoGallery.vue
@@ -1,24 +1,10 @@
 <template>
-  <!-- <div v-for="videoId in videoIds" :key="videoId" class="video-gallery"> -->
   <div class="video-gallery">
-    <!-- <VideoItem
-      v-for="(video, index) in videos"
-      :key="index"
-      :video="video"
-    /> -->
-    <!-- <div v-for="videoTitle in videoTitles" :key="videoTitle"> -->
-
-    <div v-for="videoId in videoIds" :key="videoId">
-     <!-- <div v-for="videoTitle in videoTitles" :key="videoTitle"> -->
-      <div v-html="generateYouTubeEmbedCode(videoId)" class="video-embed"></div>
-      <h3 v-html="generateYouTubeTitle(videoTitle)"></h3>
-    <!-- </div> -->
-    </div> 
-
-    <!-- </div> -->
-
+    <div v-for="(item, key) in videos" :key="key">
+      <div v-html="generateYouTubeEmbedCode(item.url)"></div>
+      <h3>{{ item.title }}</h3>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -30,11 +16,8 @@ export default {
   },
   data() {
     return {
-      // Hardcoded YouTube video IDs
-      videoIds: ['kbZejnPXyLM?si=Iu8hmadPtfMTynz3','SRec90j6lTY?si=aJVpNhfCt5E6vzrB', 'rYH7AErVd7w?si=_YAEGfSMLmDIStnc','t0aCoqXKFOU?si=j9IZqmkzSp8hPCbD','TgqeRTwZvIo?si=GVf2f_d2rUW36jd7','c9Wg6Cb_YlU?si=n8m_HCn0QXpmMIeC','an5LH3K44ks?si=aSOujKAemhAyoZ8c','BZ0QER_0ZWI?si=rs5Sw7yq9qYQz4cx','uL2ZB7XXIgg?si=UteNqhgxzbU7wksA','D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx','fYs2Mdyasuc?si=VpZs941UHD_oXuK_','MkKr2Cc0BuM?si=wpAAbxKViT2xP8ZR'],
-      videoTitles: ['User Research', 'Information Architecture', 'User Personas', 'User Journey Mapping', 'Visual Design', 'Prototyping', 'Interaction Design', 'Responsive Design', 'Cognitive Psychology', 'Emotional Design', 'Usability', 'Business Understanding', 'Market Research', 'Collaboration', 'Ethics and Legal Consideration'],
       videos: [
-        // Add video URLs or other information here
+        // Add YouTube video IDs title description here
         { url: 'kbZejnPXyLM?si=Iu8hmadPtfMTynz3', title: 'User Research' },
         { url: 'SRec90j6lTY?si=aJVpNhfCt5E6vzrB', title: 'Information Architecture' },
         { url: 'rYH7AErVd7w?si=_YAEGfSMLmDIStnc', title: 'User Personas' },
@@ -45,21 +28,18 @@ export default {
         { url: 'BZ0QER_0ZWI?si=rs5Sw7yq9qYQz4cx', title: 'Responsive Design' },
         { url: 'uL2ZB7XXIgg?si=UteNqhgxzbU7wksA', title: 'Cognitive Psychology' },
         { url: 'D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx', title: 'Emotional Design' },
-        { url: 'D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx', title: 'Usability' },
-        { url: 'D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx', title: 'Business Understanding' },
-        { url: 'D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx', title: 'Market Research' },
-        { url: 'D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx', title: 'Collaboration' },
-        { url: 'D4NyQ5iOMF0?si=0eFiyg6-NFRJDwgx', title: 'Ethics and Legal Consideration' }
+        { url: 'fYs2Mdyasuc?si=VpZs941UHD_oXuK_', title: 'Usability' },
+        { url: 'MkKr2Cc0BuM?si=wpAAbxKViT2xP8ZR', title: 'Business Understanding' },
+        { url: 'ULWhr_2CP-g?si=JzpfkV7TY8yAl5Z2', title: 'Market Research' },
+        { url: 'DwR0SZTBMTU?si=p54QB5dx8COMAcW_', title: 'Collaboration' },
+        { url: '8I3NTE4cn5s?si=bTItTFQX4Bumhogm', title: 'Ethics and Legal Consideration' }
       ]
   } 
     },
     methods: {
-      generateYouTubeEmbedCode(videoId,videoTitle) {
+      generateYouTubeEmbedCode(videoId) {
       return `<iframe width="350" height="200" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe><br>`;
-    },
-      generateYouTubeTitle(videoTitle) {
-      return `<h3>${videoTitle}</h3>`;
-    },
+    }
   }
 };
 </script>
