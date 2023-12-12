1ab3ed989ec7e0aa4f9f7a7a64832c79d7ee6546
James Hooper <jameshooperdesigns@gmail.com>
2023-12-11 22:20:48 -0700

    Add testimonial slider with profile images

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="testimonial-slider">
     <div class="testimonial" v-for="(testimonial, index) in testimonials" :key="index" :class="{ active: index === currentIndex }">
-      <p>{{ testimonial.message }}</p>
-      <p class="author">{{ testimonial.author }}</p>
+      <div class="testimonial-content">
+        <p>{{ testimonial.message }}</p>
+        <p class="author">{{ testimonial.author }}</p>
+      </div>
+      <img :src="testimonial.profilePicture" alt="Profile Picture" class="profile-picture" />
     </div>
     <div class="slider-controls">
       <button @click="prevTestimonial" :disabled="currentIndex === 0">Previous</button>
@@ -20,14 +23,17 @@ export default {
         {
           message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
           author: "John Doe",
+          profilePicture: "https://placehold.co/200x200",
         },
         {
           message: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
           author: "Jane Smith",
+          profilePicture: "phttps://placehold.co/200x200",
         },
         {
           message: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
           author: "Alice Johnson",
+          profilePicture: "https://placehold.co/200x200",
         },
       ],
     };
@@ -46,7 +52,7 @@ export default {
 <style scoped>
 .testimonial-slider {
   position: relative;
-  max-width: 600px;
+  max-width: 700px;
   margin: 0 auto;
 }
 
@@ -56,15 +62,31 @@ export default {
   border: 1px solid #ccc;
   border-radius: 5px;
   background-color: #f9f9f9;
+  position: relative;
 }
 
 .testimonial.active {
   display: block;
 }
 
+.testimonial-content {
+  margin-bottom: 15px;
+  max-width: 550px;
+}
+
 .author {
   margin-top: 10px;
   font-style: italic;
+}
+
+.profile-picture {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+  position: absolute;
+  top: 10px;
+  right: 10px;
 }
 
 .slider-controls {
