dc86d0fefee3e142eaecf51e1ad49c79a424cfed
James Hooper <jameshooperdesigns@gmail.com>
2023-12-14 23:52:10 -0700

    Add tests to the router file

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,6 +34,14 @@ const router = createRouter({
       component: () => import('../views/Templates.vue')
     },
     {
+      path: '/tests',
+      name: 'tests',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Tests.vue')
+    },
+    {
       path: '/contact',
       name: 'contact',
       // route level code-splitting
---
+++ src/views/Tests.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header />
+  <div class="about">
+    <h1>Test your knowledge.</h1>
+  </div>
+  <Footer />
+</template>
+
+<style>
+
+.about {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #090909;
+  height: 90vh;
+}
+</style>
+
+
+<script>
+import Header from "@/components/Header.vue";
+import TestTwo from "@/components/TestTwo.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    TestTwo,
+    Footer,
+  }
+}
+</script>
